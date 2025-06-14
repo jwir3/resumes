0f943827f1c876b725f4e8c1ef984d0445bf5bc3
Scott Johnson <jaywir3@gmail.com>
2025-06-14 11:45:04 -0500

    :bug: Fixed a script issue when running on Github Actions.

--- compile.sh
+++ compile.sh
@@ -27,7 +27,7 @@ fi
 if [ ! -d "styles/$STYLE" ]; then
   echo "style '$STYLE' doesn't exist, using default" >&2
   STYLE=default
-else 
+else
   echo "using '$STYLE' style" >&2
 fi
 
@@ -35,7 +35,7 @@ fi
 source_base=$(basename "$sourcefile" .md)
 
 # shellcheck disable=SC2012
-styles=$(ls -p styles/${STYLE}/*.css | sed "s/^/-c /" | tr "\n" " ")
+styles=$(ls -p "styles/${STYLE}/*.css" | sed "s/^/-c /" | tr "\n" " ")
 
 # we actually want the spaces to split out the args
 # shellcheck disable=SC2086
